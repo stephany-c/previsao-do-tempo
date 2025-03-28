0eb39b13c969b672d0a8a3aa71b272f0220de306
stephany-c <ster.carolinas@hotmail.com>
2025-03-27 23:09:44 -0300

    inserindo dados de previsão

--- src/components/WeatherInformations/WeatherInformation.jsx
+++ src/components/WeatherInformations/WeatherInformation.jsx
@@ -1,12 +1,34 @@
+
+
 function WeatherInformation({ weather }) {
     console.log(weather)
+
+    if (!weather || !weather.main) {
+        return <p>Insira o local que queira saber o clima</p>;
+    }
     return (
         <div className="WeatherInformation" >
-            <h1>Componente</h1>
-        </div>)
+            <h1>{weather.name}</h1>
+
+            {weather.visibility}
+            <p>Temperatura: {weather.main.temp}°C</p>
+            <p>Temperatura: {weather.main.temp}°C</p>
+            <p>Sensação térmica: {weather.main.feels_like}°C</p>
+            <p>Umidade: {weather.main.humidity}%</p>
+            <p>Pressão: {weather.main.pressure} hPa</p>
+
+
+        </div>
+
+
+
+
+
+
+
+    )
 
 }
-
 
 
 export default WeatherInformation
