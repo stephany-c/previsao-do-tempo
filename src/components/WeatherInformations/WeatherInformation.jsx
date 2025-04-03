1adb969db844e7f41160e2272f6b9bf8300468be
stephany-c <ster.carolinas@hotmail.com>
2025-04-03 19:19:47 -0300

    ajuste de css

--- src/components/WeatherInformations/WeatherInformation.jsx
+++ src/components/WeatherInformations/WeatherInformation.jsx
@@ -1,33 +1,43 @@
-import './WeatherInformation.css'
+import './WeatherInformation.css';
 
 function WeatherInformation({ weather }) {
+    // Converte os timestamps da API para um formato legível
+    const formatTime = (timestamp) => {
+        return new Date(timestamp * 1000).toLocaleTimeString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit"
+        });
+    };
+
+    const lastUpdate = formatTime(weather.dt);
+    const sunriseTime = formatTime(weather.sys.sunrise);
+    const sunsetTime = formatTime(weather.sys.sunset);
+
     return (
-        <div className="weather-container" >
+        <div className="weather-container">
             <h2>{weather.name}</h2>
+            <p className="update-time">Última atualização: {lastUpdate}</p>
+
             <div className='weather-info'>
                 <img
-                    alt='icone-tempo' src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} />
+                    alt='icone-tempo'
+                    src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+                />
                 <p className='temperature'>{Math.round(weather.main.temp)}°C</p>
             </div>
+
             <div>
                 <p className='description'>{weather.weather[0].description}</p>
             </div>
+
             <div className='details'>
-                <div><p>Sensação térmica:{weather.main.feels_like}°C</p></div>
+                <div><p>Sensação térmica: {Math.round(weather.main.feels_like)}°C</p></div>
                 <div><p>Umidade: {weather.main.humidity}%</p></div>
                 <div><p>Pressão: {weather.main.pressure} hPa</p></div>
+
             </div>
         </div>
-
-
-
-
-
-
-
-    )
-
+    );
 }
 
-
-export default WeatherInformation
+export default WeatherInformation;
--- src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
+++ src/components/WeatherInformations5Days/WeatherInformations5Days.jsx
@@ -15,18 +15,28 @@ function WeatherInformation5Days({ weather5Days }) {
 
     return (
         <div className="weather-container">
-            <p>Previsão próximos 5 dias</p>
-            {next5DaysForecast.map((forecast) => (
-                <div key={forecast.dt}>
-                    <p>{new Date(forecast.dt * 1000).toLocaleDateString("en-US", { weekday: "long" })}</p>
-                    <img
-                        src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
-                        alt={forecast.weather[0].description}
-                    />
-                    <p>{forecast.weather[0].description}</p>
-                    <p>{forecast.main.temp_min}°C min / {forecast.main.temp_max}°C máx</p>
-                </div>
-            ))}
+            <h3>Previsão próximos 5 dias</h3>
+
+            <div className='weather-list'>
+                {next5DaysForecast.map((forecast) => {
+                    const dateObj = new Date(forecast.dt * 1000);
+                    const dayOfWeek = dateObj.toLocaleDateString("pt-BR", { weekday: "long" });
+                    const dayNumber = dateObj.toLocaleDateString("pt-BR", { day: "numeric" });
+
+                    return (
+                        <div className='weather-item' key={forecast.dt}  >
+                            <p>{dayOfWeek}, {dayNumber}</p>
+                            <img
+                                src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+                                alt={forecast.weather[0].description}
+                            />
+                            <p>{forecast.weather[0].description}</p>
+                            <p>{Math.round(forecast.main.temp_min)}°C min / {Math.round(forecast.main.temp_max)}°C máx</p>
+                        </div>
+                    );
+                })}
+
+            </div>
         </div>
     );
 }
